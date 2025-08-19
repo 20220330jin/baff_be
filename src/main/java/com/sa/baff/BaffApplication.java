71009372ac1217274fee7e5ba22d65a24f370e91
hjkim <20220330jin@gmail.com>
2025-08-19 17:53:32 +0900

    init project

---
+++ src/main/java/com/sa/baff/BaffApplication.java
@@ -0,0 +1,13 @@
+package com.sa.baff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaffApplication.class, args);
+	}
+
+}
