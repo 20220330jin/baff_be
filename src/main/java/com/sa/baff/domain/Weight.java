f22cc3b2c7ecbff50e3d12cc6ef7ff9a3fc7963c
young jo <a10019610@gmail.com>
2025-08-26 14:28:33 +0900

    feat: 체중 기록 업데이트
    체중 기록 중복처리를 하여 체중 기록을 업데이트 합니다.

--- src/main/java/com/sa/baff/domain/Weight.java
+++ src/main/java/com/sa/baff/domain/Weight.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Getter
+@Setter
 public class Weight extends BaseEntity {
 
     @Id
