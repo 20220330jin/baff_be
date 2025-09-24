813cb37f344ddc2acad3c21e5199f4ec869f1c77
hjkim <20220330jin@gmail.com>
2025-09-24 12:58:44 +0900

    feat: time zone

--- src/main/java/com/sa/baff/BaffApplication.java
+++ src/main/java/com/sa/baff/BaffApplication.java
@@ -1,13 +1,22 @@
 package com.sa.baff;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BaffApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaffApplication.class, args);
-	}
+    @PostConstruct
+    public void started() {
+        // JVM의 시간대를 한국으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaffApplication.class, args);
+    }
 
 }
